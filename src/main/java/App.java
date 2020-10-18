435ec2ed5243064b83415abbe7d0aae26ab248b2
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-10-18 15:47:51 +0300

    add README.md

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,21 @@
 
 
+//public class App {
+//
+//}
+import models.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
+    public static void main(String[] args) {
 
+
+    }
 }
-
